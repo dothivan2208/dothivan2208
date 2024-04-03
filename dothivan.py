fea7a61dbe2aef7980cbc56015baf4e84529222c
dothivan2208 <ftdothivan2208@gmail.com>
2024-04-03 17:52:42 +0700

    commit 1

---
+++ dothivan.py
@@ -0,0 +1,9 @@
+# Đề bài
+# - Đọc 1 file văn bản gồm nhiều dòng
+# - Ghi ra màn hình theo thứ tự ngược lại của các dòng
+# ví du: 
+# Nam quốc sơn hà 
+# Nam đế cư
+# ==>
+# Nam đế cư
+# Nam quốc sơn hà
